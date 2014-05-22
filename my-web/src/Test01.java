e59422ef54fa49b7f439059d3f93cefc06de0c01
web1992 <554933654@qq.com>
2014-05-22 09:07:25 +0800

    5-22-test

---
+++ my-web/src/Test01.java
@@ -0,0 +1,9 @@
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageDecoder;
+
+
+public class Test01 {
+	public static void main(String[] args) {
+		JPEGImageDecoder j;
+	}
+}	
